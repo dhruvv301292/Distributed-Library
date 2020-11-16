495ea274be47049d1ba92a9a95a85a2e01a2564e
Aiswarya Vinod Kumar <37078306+aiskumo@users.noreply.github.com>
2020-11-16 01:28:46 -0500

    Add files via upload

--- rm.py
+++ rm.py
@@ -3,39 +3,39 @@ import time
 import argparse
 import pickle
 
+
+
 def save_data(data):
     #Storing data with labels
-    a_file = open('membership.pkl', "wb")
+    a_file = open('rm_membership_rm.pkl', "wb")
     pickle.dump(data, a_file)
     a_file.close()
 
-
-
-class GFD:
+class RM:
     def __init__(self, ips, freq):
         self.heartbeat_count = 0
         self.ips = ips
         self.num_server = len(ips)
         self.alive = [False]*self.num_server 
         self.freq = freq
-        print('GFD:', sum(self.alive), 'members\n')
+        print('RM:', sum(self.alive), 'members\n')
         save_data(self.alive)
-        
-        
+
     def heartbeat(self):
         start = time.time()
         check = True
 
-        
         while check:
             self.heartbeat_count += 1
+
+            #TODO: remove heartbeat for RM
             
             for i, ip in enumerate(self.ips):
                 print ( "[{}] | beatCount: {} sending heartbeat".format (time.strftime ( "%H:%M:%S", time.localtime () ), self.heartbeat_count ) )
-                
+
                 try:
                     resp = req.get(ip + "heartbeat")
-                    print("[{}] | beatCount: {} | received response from lfd{}: {}".format(time.strftime("%H:%M:%S", time.localtime()), self.heartbeat_count, i+1, resp.text))
+                    print("[{}] | beatCount: {} | received response from GFD{}: {}".format(time.strftime("%H:%M:%S", time.localtime()), self.heartbeat_count, i+1, resp.text))
                     if resp.text == 'True' and self.alive[i] != True:
                         self.alive[i] = True
                         print('Added Member S' + str(i+1) )
@@ -43,30 +43,19 @@ class GFD:
                         self.alive[i] = False
 
                 except:
-                    print("[{}] | beatCount: {}: lfd{} not available".format (time.strftime("%H:%M:%S", time.localtime()), self.heartbeat_count, i+1))
+                    print("[{}] | beatCount: {}: GFD not available".format (time.strftime("%H:%M:%S", time.localtime()), self.heartbeat_count))
                     self.alive[i] = False
-
 
             alive_members = ['S'+str(i) for i in range(len(self.alive)) if self.alive[i] == True]
             members = ",".join(alive_members)
-            print('GFD:', sum(self.alive), 'members:', members,'\n')
+            print('RM:', sum(self.alive), 'members:', members,'\n')
             time.sleep ( self.freq - ((time.time () - start) % self.freq) )
             save_data(self.alive)
-            #TODO: Send update from GFD to RM here. 
             print()
-            
-            
-
-def heartbeat_replicas(lfd):
-    lfd.heartbeat()
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser ()
-
-    #TODO: setup port number for GFD to RM
-
-    parser.add_argument('--ports', type=int, default=[5010, 5011, 5012], nargs='+')
+    parser.add_argument('--ports', type=int, default=[5020], nargs='+') # port number of GFD will be 5020 here.
     parser.add_argument('--freq', type=float, default=1.0)
     args = parser.parse_args ()
 
@@ -76,8 +65,7 @@ if __name__ == '__main__':
     for port in args.ports:
         ips.append('http://127.0.0.1:' + str(port) + '/')
     print(ips)
-
-    gfd = GFD(ips, args.freq)
-    gfd.heartbeat()
-
+    
+    rm = RM(ips, args.freq)
+    rm.heartbeat()
 
